07f08ef4427a1e58aec94880622fbba85bfe1f84
Soham Thakur <thakursoham131@gmail.com>
2025-07-02 01:03:51 +0530

    Login and Signup Page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,8 @@ import SearchDiscover from "./SearchDiscover";
 import BookingOversight from "./BookingOversight";
 import Dashboard from "./Dashboard";
 import OfficialDetails from "./OfficialDetails";
-
+import Login from "./Login";
+import Register from "./Register";
 const App = () => {
   return (
     <Router>
@@ -17,13 +18,15 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />{" "}
           {/* Default route - HomePage */}
-          <Route path="/register" element={<RegisterForm />} />
+          {/* <Route path="/register" element={<RegisterForm />} /> */}
           <Route path="/complete-profile" element={<CompleteProfile />} />
           <Route path="/admin-panel" element={<AdminPanel />} />
           <Route path="/search-discover" element={<SearchDiscover />} />
           <Route path="/booking-oversight" element={<BookingOversight />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/official-details" element={<OfficialDetails />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
         </Routes>
       </div>
     </Router>
