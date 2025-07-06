d94ca07a1a233fd3a3f8bc472890b817beeee9f8
Shray0611 <164644420+Shray0611@users.noreply.github.com>
2025-07-06 16:54:15 +0530

    Directory Structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
-import CompleteProfile from "./CompleteProfile";
-import AdminPanel from "./AdminPanel";
-import OrganizerNavbar from "./OrganizerNavbar";
-import SearchOfficials from "./SearchOfficials";
-import OrgDashboard from "./OrgDashboard"
+import HomePage from "./Home/HomePage";
+import AdminPanel from "./admin/AdminPanel";
+import OrganizerNavbar from "./organizers/OrganizerNavbar";
+import SearchOfficials from "./organizers/SearchOfficials";
+import OrgDashboard from "./organizers/OrgDashboard";
 
-import Login from "./Login";
-import Register from "./Register";
-import OfficalsHomepage from "./OfficalsHomepage";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import OfficalsHomepage from "./officials/OfficalsHomepage";
 const App = () => {
   return (
     <Router>
@@ -18,9 +17,9 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/" element={<HomePage />} />{" "}
           {/* Default route - HomePage */}
-          <Route path="/complete-profile" element={<CompleteProfile />} />
           <Route path="/admin-panel" element={<AdminPanel />} />
-          <Route path="/OrgDashboard" element={<OrgDashboard />} /> {/* updated route */}
+          <Route path="/OrgDashboard" element={<OrgDashboard />} />{" "}
+          {/* updated route */}
           <Route path="/search-officials" element={<SearchOfficials />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
